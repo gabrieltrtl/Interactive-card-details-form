55a98781ee5a4c5d6428662cab1bb1b4d9b1c4d1
Gabriel Lira <gabriel.tertulino@proton.me>
2024-06-12 11:33:50 -0300

    commit 8.0

--- scripts.js
+++ scripts.js
@@ -2,19 +2,24 @@ const cardForm = document.querySelector('.hero__form');
 console.log(cardForm);
 const formInputs = document.querySelectorAll('.hero__form input')
 console.log(formInputs);
+const errorMsg = document.querySelectorAll('.error-msg');
+console.log(errorMsg);
 
 
 cardForm.addEventListener('submit', checkInputs);
 
-function showError(errorMessage) {
-  
+function showError(input, errorMessage) {
+  errorMsg.forEach((error) => {
+    error.style.display = "block";
+    error.textContent = errorMessage;
+  })
 }
 
 function checkInputs(event) {
+  event.preventDefault();
   formInputs.forEach((input) => {
     if(input.validity.valueMissing) {
-      showError("Can't be blank");
+      showError(input, "Can't be blank");
     }
   })
-  event.preventDefault();
 }
