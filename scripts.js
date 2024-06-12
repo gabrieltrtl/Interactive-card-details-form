0f704b39b05ee8c7fa9a89807d3eaa431086623a
Gabriel Lira <gabriel.tertulino@proton.me>
2024-06-12 19:32:58 -0300

    commit 10.0

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const formInputs = document.querySelectorAll('.hero__form input')
 console.log(formInputs);
 const errorMsg = document.querySelectorAll('.error-msg');
 console.log(errorMsg);
+const completedState = document.querySelector('.completed')
 
 
 cardForm.addEventListener('submit', checkInputs);
@@ -19,15 +20,26 @@ function formSent(){
 
 function checkInputs(event) {
   event.preventDefault();
+  let isFormValid = true;
+
   formInputs.forEach((input) => {
     if(input.validity.valueMissing) {
       showError(input.nextElementSibling, "Can't be blank");
+      isFormValid = false;
     } else if (input.id === 'card-name' && !/^[a-zA-ZÀ-ÿ\s]+$/.test(input.value)) {
       showError(input.nextElementSibling, "Only letters allowed");
+      isFormValid = false;
     } else if (input.validity.tooShort && /[0-9]/.test(input.value)) {
-      showError(input.nextElementSibling, "Invalid Number");
+      showError(input.nextElementSibling, "Wrong Format");
+      isFormValid = false;
     } else if (/[^0-9]/.test(input.value) && input.id !== 'card-name') {
       showError(input.nextElementSibling, "Wrong format, numbers only");
+      isFormValid = false;
     }
-  })
+  });
+
+  if (isFormValid) {
+    cardForm.style.display = 'none';
+    completedState.style.display = 'flex';
+  }
 }
